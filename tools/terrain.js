78766da9d6cb67cc1077eb6573d7bf404d326bfd
smldms <97317400+smldms@users.noreply.github.com>
2022-12-29 15:57:47 +0100

    Update terrain.js

--- tools/terrain.js
+++ tools/terrain.js
@@ -26,19 +26,3 @@ function terrain(maxH, relief, clr1, clr2, dens, factor, sclX, sclY) {
         endShape(CLOSE);
     }
 }
-
-function layers() {
-    let l1 = {
-        maxH: 0,
-        relief: 0.05,
-        clr1: color(0),
-        clr2: color(255),
-        dens: height,
-        factor: 0.02,
-        sclX: 5,
-        sclY: 5,
-    }
-
-    terrain(...Object.values(l1));
-
-}
